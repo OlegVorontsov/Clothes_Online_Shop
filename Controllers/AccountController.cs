d657a1b7e5dd008a5d60dd2056e68327898bfa2c
Олег Воронцов <oleg-voroncov@mail.ru>
2024-04-24 11:48:55 +0300

    Login View made

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Clothes_Online_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Online_Shop.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login loginInfo)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Clothes_Online_Shop.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
